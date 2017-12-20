d5e03050152268e6474fe01645e9b22df8c1fbb8
laszlocseh <csehlaszlo91@gmail.com>
2017-12-20 12:33:36 +0200

    some changes

--- convert.py
+++ convert.py
@@ -2,22 +2,28 @@ from pyexcel_xls import get_data
 from lxml import etree
 import re
 
-result = get_data(afile='emissions1.xls')
+# settings
+inputFile = "LCP_extract_v3.1_xlsx.xlsx"
+outputFile = "LCP_v2_plantsdb.xml"
+sheetsToTransform = ["2015"]
+mainElementName = "Plant"
 
+inputFileData = get_data(afile=inputFile)
 xml = etree.Element('dataroot')
 root = etree.ElementTree(xml)
 
-for sheet in result:
-    nodeNames = result[sheet][0]
-    for values in result[sheet][1:]:
-        node = etree.SubElement(xml, 'emissions')
+
+for sheet in sheetsToTransform:
+    nodeNames = inputFileData[sheet][0]
+    for values in inputFileData[sheet][1:]:
+        node = etree.SubElement(xml, mainElementName)
         for index, nodeValue in enumerate(values):
-            tmp = re.sub(r'[\W\s]', '', nodeNames[index])
-            item = etree.SubElement(node, tmp)
+            nodeNameNormalized = re.sub(r'[\W\s]', '', nodeNames[index])
+            elem = etree.SubElement(node, nodeNameNormalized)
             if isinstance(nodeValue, float):
-                item.text = "%.2f" % nodeValue
+                elem.text = "%.2f" % nodeValue
             else:
-                item.text = str(nodeValue)
-        item = etree.SubElement(node, "Year")
-        item.text = "2015"
-    root.write("output.xml", encoding=None, method="xml", pretty_print=True)
+                elem.text = str(nodeValue)
+        # item = etree.SubElement(node, "Year")
+        # item.text = "2015"
+    root.write(outputFile, encoding=None, method="xml", pretty_print=True)
