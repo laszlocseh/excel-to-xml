14b27a6621a8480c08aa92c649d5119624b1e419
laszlocseh <csehlaszlo91@gmail.com>
2018-03-28 11:36:28 +0300

    minor changes

--- convert.py
+++ convert.py
@@ -5,44 +5,49 @@ import os
 import datetime
 
 # settings
-inputFile = os.path.join("inputs", "TEST-COMPANIES-Licences.xlsx")
-outputFile = os.path.join("outputs", "test-companies-new.xml")
-sheetsToTransform = ("Sheet2", )
-headersToTransform = ()
-headersToNotTransform = ()
-mainElementName = "licence"
+inputFile = os.path.join("inputs", "Thematic_Lookups_forEEA.xlsx")
+outputName = "EPRTR-LCP_C12.6"
+sheetsToTransform = ("new", )
+columnsToTransform = ()
+columnsToNotTransform = ()
+dataElementName = "row"
 
+
+# init the xml file
 inputFileData = get_data(afile=inputFile)
 document_node = etree.Element('dataroot')
 root = etree.ElementTree(document_node)
-
+# set generated element in xml whith current datetime
 document_node.set("generated", str(datetime.datetime.now().replace(microsecond=0).isoformat()))
 
 
+# check if the column from excel is needed
 def node_is_needed(name):
     return (
-       not headersToTransform
-       or name in headersToTransform
+       not columnsToTransform
+       or name in columnsToTransform
        ) \
-       and name not in headersToNotTransform
+       and name not in columnsToNotTransform
 
 
 def main():
     for sheet in sheetsToTransform:
+        outputFileName = os.path.join("outputs", "{}_{}.xml".format(outputName, sheet))
         node_names = inputFileData[sheet][0]
         sheet_data_rows = inputFileData[sheet][1:]
         for row in sheet_data_rows:
-            main_element = etree.SubElement(document_node, mainElementName)
+            main_element = etree.SubElement(document_node, dataElementName)
             for index, node_value in enumerate(row):
                 node_name = node_names[index]
                 if node_is_needed(node_name):
                     node_name_normalized = re.sub(r'[\W\s]', '', node_name)
                     node = etree.SubElement(main_element, node_name_normalized)
-                    if isinstance(node_value, float):
-                        node.text = "%.4f" % node_value
-                    else:
-                        node.text = str(node_value)
-        root.write(outputFile, encoding=None, method="xml", pretty_print=True)
+                    # if isinstance(node_value, float):
+                    #     node.text = "%.5f" % node_value
+                    # else:
+                    #     node.text = str(node_value)
+                    node.text = str(node_value)
+        root.write(outputFileName, encoding=None, method="xml", pretty_print=True)
 
 
 if __name__ == "__main__":
